ca7de8ef7df08d69b1646fb3c7f48684dea19f62
Levi Huchingson <levihuchingson@gmail.com>
2020-01-11 16:26:33 -0500

    Updates

---
+++ precache-manifest.32764d9d1c1a19c4bc7f7af25aca4de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196ea881b05d6210c8553b29044fbcfc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c9b322e0f86723ee3b2",
+    "url": "/portfolio/static/css/main.b869a755.chunk.css"
+  },
+  {
+    "revision": "b1b4e02a16a3c238a31f",
+    "url": "/portfolio/static/js/2.dde26d3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.dde26d3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c9b322e0f86723ee3b2",
+    "url": "/portfolio/static/js/main.7c52406d.chunk.js"
+  },
+  {
+    "revision": "40ff3004e53559d8fe54",
+    "url": "/portfolio/static/js/runtime-main.cac2caa8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
